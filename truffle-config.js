3314bdcbeb5637033450ad2f96038120b31326a3
winnie-byun <winnie.byun@groundx.xyz>
2021-01-28 17:37:02 +0900

    change hdwalletProvider to klaytn

--- truffle-config.js
+++ truffle-config.js
@@ -1,44 +1,36 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-const MNEMONIC =
-  process.env.MNEMONIC ||
-  'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
-const INFURA_API_KEY = process.env.INFURA_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
-const walletProvider = provider => new HDWalletProvider(MNEMONIC, provider)
+const privateKey = process.env.PRIVATE_KEY
 
 module.exports = {
   //$ truffle test --network <network-name>
   networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // match any network
-      gas: 7000000,
-      skipDryRun: true,
+    baobab: {
+      provider: () => new HDWalletProvider(privateKey, "https://api.baobab.klaytn.net:8651"),
+      network_id: '1001', //Klaytn baobab testnet's network id
+      gas: '8500000',
+      gasPrice: null
     },
-    goerli: {
-      provider: () => walletProvider(`https://goerli.infura.io/v3/${INFURA_API_KEY}`),
-      network_id: 5,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true,
-    },
-    maticMumbai: {
-      provider: () => walletProvider('https://rpc-mumbai.matic.today'),
-      network_id: 80001,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true,
-    },
-    mainnet: {
-      provider: () => walletProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
-      network_id: 1,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true,
-    },
+    cypress: {
+      provider: () => new HDWalletProvider(privateKey, "https://api.cypress.klaytn.net:8651"),
+      network_id: '8217', //Klaytn mainnet's network id
+      gas: '8500000',
+      gasPrice: null
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "constantinople"
+      }
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -57,9 +49,5 @@ module.exports = {
 
   verify: {
     preamble: 'LINK\nVersion: 0.1.0',
-  },
-
-  api_keys: {
-    etherscan: ETHERSCAN_API_KEY,
   },
 }
